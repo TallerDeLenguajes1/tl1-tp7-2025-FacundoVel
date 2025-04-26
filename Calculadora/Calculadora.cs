40ed8064261125f997220eb623c2ac5b23c4188f
FacundoVel <facundovel.99@gmail.com>
2025-04-26 10:33:27 -0300

    Se crea gitignore y Calculadora.cs

---
+++ Calculadora/Calculadora.cs
@@ -0,0 +1,31 @@
+namespace EspacioCalculadora;
+
+public class Calculadora
+{
+    double dato;
+
+
+    public void Sumar(double termino)
+    {
+
+    }
+    public void Restar(double termino)
+    {
+
+    }
+
+    public void Multiplicar(double termino)
+    {
+
+    }
+
+    public void Dividir(double termino)
+    {
+
+    }
+
+    public void Limpiar()
+    {
+
+    }
+}
