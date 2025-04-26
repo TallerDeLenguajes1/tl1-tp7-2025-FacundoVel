9f79127a0d013b377751e87b000aaeae63236dce
FacundoVel <facundovel.99@gmail.com>
2025-04-26 12:19:00 -0300

    Se crean los metodos para la toma de datos

--- SisPersonal/Empleado.cs
+++ SisPersonal/Empleado.cs
@@ -12,12 +12,40 @@ namespace EspacioEmpleado{
     {
         private string nombre;
         private string apellido;
-        private DateTime fechaNacmiento;
+        private DateTime fechaNacimiento;
         private char estadoCivil;
         private DateTime fechaDeIngreso;
-        private double SueldoBasico;
-        private cargos Cargo; 
+        private double sueldoBasico;
+        private cargos cargo; 
 
-        
+        public string Nombre{
+            get => nombre;
+            set => nombre = value;
+        }
+        public string Apellido{
+            get => apellido;
+            set => apellido = value;
+        }
+        public DateTime FechaNacimiento{
+            get => fechaNacimiento;
+            set => fechaNacimiento = value;
+        }
+        public char EstadoCivil{
+            get => estadoCivil;
+            set => estadoCivil = value;
+        }
+        public DateTime FechaDeIngreso{
+            get => fechaDeIngreso;
+            set => fechaDeIngreso = value;
+        }
+        public double SueldoBasico{
+            get => sueldoBasico;
+            set => sueldoBasico = value;
+        }
+        public cargos Cargo{
+            get => cargo;
+            set => cargo = value;
+        }
+
     }
 }
